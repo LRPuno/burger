7ef034dcb9de986c58d172dad7e63620c0f68196
Luis Rishi Puno <LRPuno@gmail.com>
2018-09-20 18:39:04 -0400

    Origin Version

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+INSERT INTO burger (burger_name, devoured) VALUES ("Sexy Burger",false);
+
+INSERT INTO burger (burger_name, devoured) VALUES ("Cool As Beans Burger",false);
+
+INSERT INTO burger (burger_name, devoured) VALUES ('Mystery Meat Burger',false);
+
+
---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
